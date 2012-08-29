6200eb12fd6138d62d804d718b3712ba7e960bd4
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2012-08-29 20:16:05 +0530

    Do not do encode/decode magic since Tryton 2.4 does not need it

--- attachment.py
+++ attachment.py
@@ -10,7 +10,6 @@ try:
 except ImportError:
     hashlib = None
     import md5
-import base64
 
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection
@@ -28,12 +27,13 @@ class Attachment(ModelSQL, ModelView):
     def get_data(self, ids, name):
         res = {}
         db_name = Transaction().cursor.dbname
+        format_ = Transaction().context.pop('%s.%s' % (self._name, name), '')
         s3_conn = S3Connection(
             CONFIG['s3_access_key'], CONFIG['s3_secret_key']
         )
         for attachment in self.browse(ids):
-            value = False
-            if name == 'data_size':
+            value = None 
+            if name == 'data_size' or format_ == 'size':
                 value = 0
             if attachment.digest:
                 filename = attachment.digest
@@ -41,13 +41,13 @@ class Attachment(ModelSQL, ModelView):
                     filename = filename + '-' + str(attachment.collision)
                 filename = "/".join([db_name, filename])
                 bucket = s3_conn.get_bucket(CONFIG.options['data_s3_bucket'])
-                if name == 'data_size':
+                if name == 'data_size' or format_ == 'size':
                     key = bucket.get_key(filename)
                     value = key.size
                 else:
                     k = Key(bucket)
                     k.key = filename
-                    value = base64.encodestring(k.get_contents_as_string())
+                    value = k.get_contents_as_string()
             res[attachment.id] = value
         return res
 
@@ -62,7 +62,6 @@ class Attachment(ModelSQL, ModelView):
         )
         bucket = s3_conn.get_bucket(CONFIG.options['data_s3_bucket'])
 
-        data = base64.decodestring(value)
         if hashlib:
             digest = hashlib.md5(data).hexdigest()
         else:
@@ -98,11 +97,11 @@ class Attachment(ModelSQL, ModelView):
                     ])
                     key = Key(bucket)
                     key.key = filename
-                    key.set_contents_from_string(data)
+                    key.set_contents_from_string(value)
         else:
             key = Key(bucket)
             key.key = filename
-            key.set_contents_from_string(data)
+            key.set_contents_from_string(value)
         self.write(ids, {
             'digest': digest,
             'collision': collision,
