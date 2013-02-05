92dcf3d149f013443546da7fee76e7009d225315
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-02-05 14:16:41 +0530

    Add S3Binary field #794
    * Drop in replacement for fields.Binary
    * Bump version to 2.4.0.3

--- __tryton__.py
+++ __tryton__.py
@@ -2,7 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Amazon S3 Attachments',
-    'version': '2.4.0.2',
+    'version': '2.4.0.3',
     'author': 'Openlabs Technologies & Consulting (P) Limited',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
